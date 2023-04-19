322fdde8f390e60dcf7d9842276c299256f6566f
Nicholas <crazedglitch@yahoo.com>
2023-04-19 16:38:02 -0500

    fetch response achieved

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useEffect } from "react";
+
+const fetchUrl = "https://dog.ceo/api/breeds/image/random"
+
+function App(){
+
+    useEffect( fetchDog, [])
+
+    function fetchDog() {
+        fetch( fetchUrl )
+        .then(resp => resp.json())
+        .then( dogData => console.log(dogData) )
+    }
+
+    // function renderDog(dog) {
+    //     return (
+            
+    //     )
+    // }
+
+    return (
+        <p>"Loading..."</p>
+    )
+}
+
+export default App; 
